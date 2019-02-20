24152b3c12bfa69e73f50a42db6c167ba6bd50d8
aditiexcellence <aditi@excellencetechnologies.in>
2019-02-20 13:59:42 +0530

    assignment

--- components/first.vue
+++ components/first.vue
@@ -6,10 +6,16 @@
       <li>Aliquam tincidunt mauris eu risus.</li>
       <li>Vestibulum auctor dapibus neque.</li>
     </ul>
-    <p>
+    <p v-bind:style="{color: selected.color}">
       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
       It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
     </p>
+    <select v-model="selected">
+      <option v-bind:value="{color: 'black'}">Black</option>
+      <option v-bind:value="{color: 'red'}">Red</option>
+      <option v-bind:value="{color: 'yellow'}">Yellow</option>
+      <option v-bind:value="{color: 'blue'}">Blue</option>
+    </select>
   </div>
 </template>
 <script>
@@ -17,6 +23,13 @@ export default {
   name: "First",
   props: {
     msg: String
+  },
+  data: function() {
+    return {
+      selected: {
+       color:"black"
+      }
+    };
   }
 };
 </script>
