d2fcb3f72addb6be99bc3708318f55c38a3219b1
upexcel <rahul.excel2011@gmail.com>
2019-02-27 18:35:01 +0530

    assignment

--- src/components/form.vue
+++ src/components/form.vue
@@ -1,34 +1,57 @@
 <template>
   <div>
-    <form v-on:submit.prevent>
-      <div>
-        Name:
-        <input type="text" v-model="newElement.name">
-      </div>
-      <div>
-        Email:
-        <input type="email" v-model="newElement.email">
-      </div>
-      <div>
-        Password:
-        <input type="password" v-model="newElement.password">
-      </div>
-      <div>
-        Confirm-Password:
-        <input type="password" v-model="newElement.confirmpassword">
-      </div>
-      <div>
-        D.O.B:
-        <input type="date" v-model="newElement.dob">
-      </div>
-      <input type="checkbox" v-model="newElement.checked">Terms and Condition
-      <center>
-        <button type="button" class="btn btn-secondary" v-on:click="addUser">Submit</button>
-      </center>
-    </form>
+    <b-button @click="modalShow = !modalShow">Login form</b-button>
+    <b-modal v-model="modalShow">
+      <b-form v-on:submit.prevent>
+        <b-row>
+          <b-col lg="5">Id:</b-col>
+          <b-col lg="7">
+        <b-form-input type="number" required v-model="newElement.id"/>
+        </b-col>
+        </b-row>
+        <b-row>
+          <b-col lg="5">Name:</b-col>
+          <b-col lg="7">
+            <b-form-input type="text" v-model="newElement.name"/>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col lg="5">Email:</b-col>
+          <b-col lg="7">
+            <b-form-input type="email" v-model="newElement.email"/>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col lg="5">Password:</b-col>
+          <b-col lg="7">
+            <b-form-input type="password" v-model="newElement.password"/>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col lg="5">Confirm-Password:</b-col>
+          <b-col lg="7">
+            <b-form-input type="password" v-model="newElement.confirmpassword"/>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col lg="5">D.O.B:</b-col>
+          <b-col lg="7">
+            <b-form-input type="date" v-model="newElement.dob"/>
+          </b-col>
+        </b-row>
+        <center>
+          <input type="checkbox" v-model="newElement.checked">
+          Terms and Condition
+        </center>
+        <center>
+          <button type="button" class="btn btn-secondary" v-on:click="addUser">Submit</button>
+        </center>
+      </b-form>
+    </b-modal>
     <table>
       <thead>
         <tr>
+          <th>Id</th>
           <th>Name</th>
           <th>Email</th>
           <th>Password</th>
@@ -40,6 +63,7 @@
       </thead>
       <tbody>
         <tr v-for="(newElement,index) in sortedStore" v-bind:key="index">
+          <td>{{newElement.id}}</td>
           <td>{{newElement.name}}</td>
           <td>{{newElement.email}}</td>
           <td>{{newElement.password}}</td>
@@ -50,7 +74,7 @@
             <a href="#" v-on:click="deleteItem(index)">Delete</a>
           </td>
           <td>
-            <a href="#" v-on:click="editItem(newElement)">Edit</a>
+            <a href="#" v-on:click="editItem(newElement)" @click="modalShow = !modalShow">Edit</a>
           </td>
         </tr>
       </tbody>
@@ -70,10 +94,11 @@ export default {
       store: [],
       currentSort: "name",
       currentSortDir: "asc",
-      editIndex: false,
       pageSize: 5,
       currentPage: 1,
+      modalShow: false,
       newElement: {
+        id:"",
         email: "",
         name: "",
         dob: "",
@@ -94,19 +119,41 @@ export default {
       }
       this.currentSort = s;
     },
-    editItem(newElement) {
-      this.newElement = newElement;
+    editItem(dataToEdit) {
+      console.log(this.newElement, dataToEdit);
+       this.newElement.id = dataToEdit.id;
+      this.newElement.name = dataToEdit.name;
+      this.newElement.email = dataToEdit.email;
+      this.newElement.dob = dataToEdit.dob;
+      this.newElement.password = dataToEdit.password;
+      this.newElement.confirmpassword = dataToEdit.confirmpassword;
+      this.newElement.checked = dataToEdit.checked;
     },
     addUser: function() {
-      this.store.push(this.newElement);
-      this.newElement = {
-        name: "",
-        email: "",
-        password: "",
-        confirmpassword: "",
-        dob: "",
-        checked: ""
-      };
+      if (this.index!=null) {
+        this.store.splice( 1, this.newElement);
+         console.log("mjmjm");
+        this.newElement = {
+          id:"",
+          name: "",
+          email: "",
+          password: "",
+          confirmpassword: "",
+          dob: "",
+          checked: ""
+        };
+      } else {
+        this.store.push(this.newElement);
+        this.newElement = {
+          id:"",
+          name: "",
+          email: "",
+          password: "",
+          confirmpassword: "",
+          dob: "",
+          checked: ""
+        };
+      }
     },
     nextPage: function() {
       if (this.currentPage * this.pageSize < this.store.length)
@@ -114,6 +161,9 @@ export default {
     },
     prevPage: function() {
       if (this.currentPage > 1) this.currentPage--;
+    },
+    doMath: function () {
+      return index+1
     }
   },
   computed: {
@@ -138,11 +188,9 @@ export default {
 </script>
 
 <style>
-div {
-  margin: 22px;
-}
-
-table,th,td {
+table,
+th,
+td {
   border: 1px solid black;
   padding-left: 20px;
   padding-right: 20px;
