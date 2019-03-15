e2794d2b9e3fa2a3809620ca859934142689ec2f
upexcel <rahul.excel2011@gmail.com>
2019-03-15 15:40:07 +0530

    formatting

--- App.vue
+++ App.vue
@@ -6,10 +6,10 @@
 </template>
 
 <script>
-import Counter from './components/counter.vue'
+import Counter from "./components/counter.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Counter
   }
@@ -18,8 +18,7 @@ export default {
 
 <style>
 #app {
-
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
